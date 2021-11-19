ab2f4a654d7ada6fdfcbe6c0895cfae9f75c6d7f
trikunc <kurookun@gmail.com>
2021-11-19 08:33:18 +0700

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const next = require('next');
+const cors = require('cors');
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
@@ -24,6 +25,14 @@ require('dotenv').config();
 connectDb();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 // Socket Io
 io.on('connection', (socket) => {
