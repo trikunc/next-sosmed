5f70c3e1469e8ced55cd2b25b11192c46d5a75c9
Septiana Trikuncoro <kurokunc@gmail.com>
2023-06-08 17:34:57 +0700

    change base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000'
-    : 'https://kunc-medsos.herokuapp.com';
+    : 'https://bbpvp-inovasi.netlify.app/';
 
 module.exports = baseUrl;
