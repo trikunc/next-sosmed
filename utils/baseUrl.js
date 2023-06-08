1773b994f46941970f3d04a54d4fe4feb71a7100
Septiana Trikuncoro <kurokunc@gmail.com>
2023-06-08 19:24:06 +0700

    change base url 4

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000'
-    : 'https://bbpvp-bekasi-inovasi-ftvvd7fj5-trikunc.vercel.app/';
+    : 'https://bbpvp-bekasi-inovasi-git-main-trikunc.vercel.app';
 
 module.exports = baseUrl;
