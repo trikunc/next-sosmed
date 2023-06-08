88aaeff60f6e451160d3e074ccdce42b32fd5b14
Septiana Trikuncoro <kurokunc@gmail.com>
2023-06-08 19:07:34 +0700

    test next.config

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3000'
-    : 'https://bbpvp-inovasi.netlify.app';
+    : 'https://bbpvp-inovasi.netlify';
 
 module.exports = baseUrl;
